be3e81250c14760308830ac555ba1a5bee72c56a
Lokesh Sharma <101825375+Lokessshh@users.noreply.github.com>
2023-12-09 23:11:47 +0530

    Update index.js
    changed letters to small

--- index.js
+++ index.js
@@ -11,32 +11,32 @@ const computerMove= pickComputerMove();
 
 let result = '';
 
-if (playerMove === 'Scissors'){
-  if (computerMove === 'Rock'){
+if (playerMove === 'scissors'){
+  if (computerMove === 'rock'){
   result = 'You Lose';
-  } else if(computerMove === 'Paper'){
+  } else if(computerMove === 'paper'){
   result = 'You Win';
-  } else if(computerMove === 'Scissors')  {
+  } else if(computerMove === 'scissors')  {
   result = 'Tie';
   }
 } 
 
-else if(playerMove === 'Paper'){
-  if (computerMove === 'Rock'){
+else if(playerMove === 'paper'){
+  if (computerMove === 'rock'){
   result = 'You Win';
-  } else if(computerMove === 'Paper'){
+  } else if(computerMove === 'paper'){
   result = 'Tie';
-  } else if(computerMove === 'Scissors')  {
+  } else if(computerMove === 'scissors')  {
   result = 'You Lose';
   }
 }
 
-else if(playerMove === 'Rock'){
-  if (computerMove === 'Rock'){
+else if(playerMove === 'rock'){
+  if (computerMove === 'rock'){
     result = 'Tie';
-  } else if(computerMove === 'Paper'){
+  } else if(computerMove === 'paper'){
     result = 'You Lose';
-  } else if(computerMove === 'Scissors')  {
+  } else if(computerMove === 'scissors')  {
     result = 'You Win';
   }
 }
@@ -80,15 +80,15 @@ const randomNumber = Math.random();
 let computerMove = '';
 
 if (randomNumber >= 0 && randomNumber<1/3){
-  computerMove = 'Rock';
+  computerMove = 'rock';
 }
 
 else if(randomNumber>=1/3 && randomNumber<2/3){
-  computerMove = 'Paper';
+  computerMove = 'paper';
 } 
 
 else if(randomNumber>=2/3 && randomNumber<1 ) {
-  computerMove = 'Scissors';
+  computerMove = 'scissors';
 }
 
 return computerMove;
